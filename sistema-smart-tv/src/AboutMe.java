d12b0282bd38fbe1be8a02f48d935d09fceb03e3
HansClamp <hansclamp@gmail.com>
2024-05-17 16:16:09 -0300

    Catch

--- sistema-smart-tv/src/AboutMe.java
+++ sistema-smart-tv/src/AboutMe.java
@@ -1,9 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        //criando o objeto scanner
+        try{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu nome");
@@ -23,7 +24,10 @@ public class AboutMe {
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
-        
+        }
+        catch (InputMismatchException e){
+            System.out.println("Os campos idade e altura percisam ser numéricos");
+        }
         
     }
 }
